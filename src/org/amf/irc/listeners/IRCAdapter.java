32a34e1a816b19b7888f4d4aa24b3b4dbe96e372
mfrancis95 <mikefrancis95@gmail.com>
2015-05-27 16:04:52 -0400

    Added an adapter for all of the listeners so far.

---
+++ src/org/amf/irc/listeners/IRCAdapter.java
@@ -0,0 +1,11 @@
+package org.amf.irc.listeners;
+
+import org.amf.irc.IRCClient;
+
+public abstract class IRCAdapter implements IRCChatListener, IRCJoinListener, IRCPingListener {
+    
+    public void onJoin(IRCClient client, String channel, String user) {}
+
+    public void onMessage(IRCClient client, String from, String to, String message) {}
+
+}
